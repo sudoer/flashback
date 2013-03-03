6ebde5163501e0193d70d11d2bcfa6eff0ab3f74
root <root@sheeva>
2013-03-03 18:41:13 -0500

    ready for multi-host test

--- rsback.py
+++ rsback.py
@@ -6,26 +6,31 @@ import subprocess
 import shlex
 import glob
 import operator
+import sys
+from optparse import OptionParser
 
 volumeInfo = [
-   { 'user':'root',  'host':'aspire',  'name':'etc',     'path':'/etc',     'disabled':True,  },  # small
-   { 'user':'root',  'host':'aspire',  'name':'home',    'path':'/home',    'disabled':True,  },  # 46G
-   { 'user':'root',  'host':'aspire',  'name':'boot',    'path':'/boot',    'disabled':True,  },  # small
-   { 'user':'root',  'host':'bender',  'name':'root',    'path':'/',                          },  # 2G
-   { 'user':'root',  'host':'bender',  'name':'boot',    'path':'/boot',    'disabled':True,  },  # small
-   { 'user':'root',  'host':'bender',  'name':'home',    'path':'/home',    'disabled':True,  },  # 13G
-   { 'user':'root',  'host':'bender',  'name':'backup',  'path':'/backup',  'disabled':True,  },  # 44G
-   { 'user':'root',  'host':'bender',  'name':'pub',     'path':'/pub',     'disabled':True,  },  # small
-   { 'user':'root',  'host':'bender',  'name':'copy',    'path':'/copy',    'disabled':True,  },  # 124G
-   { 'user':'root',  'host':'enigma',  'name':'home',    'path':'/home',    'disabled':True,  },  # small
-   { 'user':'root',  'host':'kimono',  'name':'users',   'path':'/Users',   'disabled':True   },  # 95G
-   { 'user':'root',  'host':'kimono',  'name':'x',       'path':'/x',       'disabled':True,  },  # 203G
-   { 'user':'root',  'host':'mini',    'name':'users',   'path':'/Users',   'disabled':True,  },  # 46G
-   { 'user':'root',  'host':'sheeva',  'name':'root',    'path':'/',        'disabled':True,  },  # 2G
-   { 'user':'root',  'host':'sheeva',  'name':'boot',    'path':'/boot',    'disabled':True,  },  # small
-   { 'user':'root',  'host':'xps',     'name':'home',    'path':'/home',    'disabled':True,  },  # 35G
-   { 'user':'root',  'host':'xps',     'name':'etc',     'path':'/etc',     'disabled':True,  },  # small
-   { 'user':'root',  'host':'xps',     'name':'boot',    'path':'/boot',    'disabled':True,  },  # small
+#  { 'host':'bender',  'name':'root',    'src':'sheeva@bender::rootfs/',  'pw': 'eb3062a3e413cdaa377548e8742a1cd6', },  # 2G
+#  { 'host':'bender',  'name':'test',    'src':'/etc',                  'minAge':86400    },  # small
+#  { 'host':'kimono',  'name':'test',    'src':'/Users/alan/iphone',    'minAge':86400    },  # 1G
+   { 'host':'aspire',  'name':'etc',     'src':'root@aspire:/etc/',     'minAge':86400    },  # small
+   { 'host':'aspire',  'name':'home',    'src':'root@aspire:/home/',    'minAge':86400    },  # 46G
+   { 'host':'aspire',  'name':'boot',    'src':'root@aspire:/boot/',    'minAge':86400    },  # small
+   { 'host':'bender',  'name':'root',    'src':'root@bender:/',         'minAge':86400    },  # small
+   { 'host':'bender',  'name':'boot',    'src':'root@bender:/boot/',    'minAge':86400    },  # small
+   { 'host':'bender',  'name':'home',    'src':'root@bender:/home/',    'minAge':86400    },  # 13G
+   { 'host':'bender',  'name':'backup',  'src':'root@bender:/backup/',  'minAge':86400    },  # 44G
+   { 'host':'bender',  'name':'pub',     'src':'root@bender:/pub/',     'minAge':86400    },  # small
+#  { 'host':'bender',  'name':'copy',    'src':'root@bender:/copy/',    'minAge':86400    },  # 124G      evaluate
+   { 'host':'enigma',  'name':'home',    'src':'root@enigma:/home/',    'minAge':86400*7  },  # small
+   { 'host':'kimono',  'name':'users',   'src':'root@kimono:/Users/',   'minAge':86400    },  # 95G
+#  { 'host':'kimono',  'name':'x',       'src':'root@kimono:/x/',       'minAge':86400    },  # 203G      evaluate
+#  { 'host':'mini',    'name':'users',   'src':'root@mini:/Users/',     'minAge':86400    },  # 46G       clean up and add
+   { 'host':'sheeva',  'name':'root',    'src':'/',                     'minAge':86400*7  },  # 2G
+   { 'host':'sheeva',  'name':'boot',    'src':'/boot/',                'minAge':86400*7  },  # small
+   { 'host':'xps',     'name':'home',    'src':'root@xps:/home/',       'minAge':86400    },  # 35G
+   { 'host':'xps',     'name':'etc',     'src':'root@xps:/etc/',        'minAge':86400    },  # small
+   { 'host':'xps',     'name':'boot',    'src':'root@xps:/boot/',       'minAge':86400    },  # small
 # XPS /dev/mapper/vg1-home         128G     35G     87G   29%  /home
 # XPS /dev/mapper/vg1-vm           109G     62G     41G   61%  /mnt/vm
 # XPS /dev/mapper/vg1-public        15G     11G    3.4G   77%  /mnt/public
@@ -37,15 +42,16 @@ volumeInfo = [
 ]
 
 top = '/rsback'
-minAge = 86400
-minAge = 600
+defaultMinAge = 86400
+options = ()
 
 # globals
 g_logFD = None
+debugMode = False
 
 #-----------------------------------------------------------
 
-def init():
+def log_init():
    # we're writing these globals
    global g_logFD
    # log file
@@ -56,6 +62,11 @@ def init():
    #  logfile = 'rsback.log'
    logfile = '/dev/stdout'
    g_logFD = open(logfile,'a')
+
+#-----------------------------------------------------------
+
+def log_debug(string):
+   if options.debug: log_info(string)
 
 #-----------------------------------------------------------
 
@@ -70,22 +81,20 @@ def log_info(string):
 
 def shell_capture(cmdargs):
    global g_logFD
-   log_info('shell_capture command >> '+(' '.join(cmdargs)))
+   log_debug('shell_capture command >> '+(' '.join(cmdargs)))
    p = subprocess.Popen(cmdargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    stdout, stderr = p.communicate()
    rc = p.returncode
-   log_info("shell_capture done, rc="+('%d'%rc))
-   log_info("")
+   log_debug("shell_capture done, rc="+('%d'%rc))
    return rc, stdout, stderr
 
 #-----------------------------------------------------------
 
 def shell_do(cmdargs):
    global g_logFD
-   log_info('shell_do command >> '+(' '.join(cmdargs)))
+   log_debug('shell_do command >> '+(' '.join(cmdargs)))
    rc = subprocess.call(cmdargs, stdout=g_logFD, stderr=g_logFD)
-   log_info("shell_do rc = "+("%d"%rc))
-   log_info("")
+   log_debug("shell_do rc = "+("%d"%rc))
    return rc
 
 #-----------------------------------------------------------
@@ -95,7 +104,16 @@ def do_backup(v):
    cfgfilename = '/tmp/rsback.conf'
    f1 = open(cfgfilename, 'w+')
 
-   # rsync_options = -val --delete --delete-excluded --stats --numeric-ids --one-file-system
+   pwopt=''
+   if 'pw' in v :
+      pwfilename = '/tmp/rsback.pw'
+      f2 = open(pwfilename, 'w+')
+      f2.write(v['pw'])
+      f2.close()
+      pwopt='--password-file '+pwfilename
+
+   # rsync_options = -v
+   # rsync_options = -E on a Mac means copy extended attributes (necessary), on Linux it means preserve exe status (innocuous)
    # exclude_file = /etc/rsback/global.exclude
    f1.write('''
 [global]
@@ -105,7 +123,7 @@ mv_cmd = /bin/mv
 rm_cmd = /bin/rm
 mkdir_cmd = /bin/mkdir
 lock_dir = /var/lock
-rsync_options = -al --delete --delete-excluded --stats --numeric-ids --one-file-system
+rsync_options = -al -E --delete --delete-excluded --stats --numeric-ids --one-file-system '''+pwopt+'''
 if_locked_retry = 3 10m
 if_error_continue = yes
 ignore_rsync_errors = 10 12 24
@@ -116,11 +134,11 @@ use_link_dest = yes
 
    f1.write('''
 [%s-%s-daily]
-source = %s@%s:%s/
+source = %s
 destination = %s/%s/%s/
 rotate = daily 9
 mode = rsync
-''' % ( v['host'], v['name'], v['user'], v['host'], v['path'], top, v['host'], v['name'] ) )
+''' % ( v['host'], v['name'], v['src'], top, v['host'], v['name'] ) )
 
    f1.write('''
 [%s-%s-weekly]
@@ -134,72 +152,106 @@ mode = link
    rc = shell_do(['./rsback-0.6.4.pl', '-v', '-c', cfgfilename, '%s-%s-daily' % (v['host'], v['name']) ])
 
    os.remove(cfgfilename)
+   if 'pw' in v : os.remove(pwfilename)
 
 #-----------------------------------------------------------
 
 # START
-init()
+def main():
 
-# LOOK FOR PID FILE, EXIT IF FOUND
+   # FIRST -- PARSE COMMAND LINE
+   usage = "usage: %prog [options]"
+   parser = OptionParser(usage)
+   parser.add_option("-d", "--debug", action="store_true", dest="debug")
+   global options
+   (options, args) = parser.parse_args()
+   if len(args) != 0:
+      parser.error("incorrect number of arguments")
 
-pidfile='/var/run/rsback-py.pid'
-try:
-   with open(pidfile) as f:
-      sys.exit()
-except IOError as e:
-   file(pidfile,'w').write(str(os.getpid()))
+   # SET UP SERVICES
 
+   log_init()
 
-# ADD SOME DEFAULTS TO 'volumeInfo' DICTIONARY
+   # LOOK FOR PID FILE, EXIT IF FOUND
 
-for volume in volumeInfo:
-   key = volume['host']+'-'+volume['name']
-   volume['key'] = key
-   volume['lastBackup'] = '2000-01-01 00:00:00'
-   if 'disabled' not in volume : volume['disabled'] = False
-
-# LOOK AT RECENT BACKUPS
-
-cmd = ['grep', '^1\\b']
-cmd.extend(glob.glob(top+'/*/*/history.daily'))
-rc, stdout, stderr = shell_capture(cmd)
-for line in stdout.split('\n'):
-   if line == '' : continue
-   junk1, junk2, right = line.partition(top)
-   pathpieces = right.split('/')
-   host = pathpieces[1]
-   name = pathpieces[2]
-   junk1, junk2, lastBackup = right.partition('\t')
-   log_info('<<'+line+'>> -> '+host+' & '+name+' & '+lastBackup)
-   # find the volumeInfo line that contains key='host-name'
+   pidfile='/var/run/rsback-py.pid'
    try:
-      idx = map(operator.itemgetter('key'), volumeInfo).index(host+'-'+name)
-      volumeInfo[idx]['lastBackup'] = lastBackup
-   except:
+      with open(pidfile) as f:
+         log_info("pidfile '%s' found... better look for a running process" % pidfile)
+         pid = int(f.readline())
+         # Check For the existence of a unix pid, send signal 0 to it.
+         try:
+            os.kill(pid, 0)
+         except OSError:
+            log_info("pid %d not found, continuing" % pid)
+         else:
+            log_info("pid %d is still running, exiting" % pid)
+            sys.exit()
+   except IOError as e:
       pass
+   file(pidfile,'w').write(str(os.getpid())+'\n')
 
-# SORT RECENT BACKUPS BY AGE
+   # ADD SOME DEFAULTS TO 'volumeInfo' DICTIONARY
 
-sortedVolumes = sorted(volumeInfo, key=operator.itemgetter('lastBackup'))
+   for volume in volumeInfo:
+      key = volume['host']+'-'+volume['name']
+      volume['key'] = key
+      volume['lastBackup'] = '2000-01-01 00:00:00'
+      if 'minAge' not in volume : volume['minAge'] = defaultMinAge
+      if 'disabled' not in volume : volume['disabled'] = False
 
-# GO THROUGH THE LIST IN ORDER, DETERMINE THEIR AGES
+   # LOOK AT RECENT BACKUPS
 
-now = datetime.datetime.now()
-for volume in sortedVolumes:
-   lastBackup = datetime.datetime.strptime(volume['lastBackup'],'%Y-%m-%d %H:%M:%S')
-   ageDelta = now - lastBackup
-   ageSeconds = ageDelta.seconds + (ageDelta.days * 86400)
-   volume['ageSeconds'] = ageSeconds
-   log_info('   '+volume['key']+' -> '+('DISABLED' if volume['disabled'] else ( volume['lastBackup']+' = '+ ('%d'%ageSeconds)) ) )
+   cmd = ['grep', '^1\\b']
+   cmd.extend(glob.glob(top+'/*/*/history.daily'))
+   rc, stdout, stderr = shell_capture(cmd)
+   for line in stdout.split('\n'):
+      if line == '' : continue
+      junk1, junk2, right = line.partition(top)
+      pathpieces = right.split('/')
+      host = pathpieces[1]
+      name = pathpieces[2]
+      junk1, junk2, lastBackup = right.partition('\t')
+      #log_debug('LINE <<'+line+'>>')
+      # find the volumeInfo line that contains key='host-name'
+      try:
+         idx = map(operator.itemgetter('key'), volumeInfo).index(host+'-'+name)
+         log_debug('host='+host+', name='+name+', lastBackup='+lastBackup+', '+host+'-'+name+' is in slot %d'%idx)
+         volumeInfo[idx]['lastBackup'] = lastBackup
+      except ValueError:
+         log_debug('host='+host+', name='+name+', lastBackup='+lastBackup+', '+host+'-'+name+' is not in the list of volumes')
+         pass
 
-# GO THROUGH THE LIST IN ORDER, BACKING UP EACH ONE IF NEEDED
+   # SORT RECENT BACKUPS BY AGE
 
-for volume in sortedVolumes:
-   if volume['ageSeconds'] < minAge : continue
-   if volume['disabled'] : continue
-   do_backup(volume)
+   sortedVolumes = sorted(volumeInfo, key=operator.itemgetter('lastBackup'))
 
-# CLEAN UP
+   # GO THROUGH THE LIST IN ORDER, DETERMINE THEIR AGES
 
-os.unlink(pidfile)
+   now = datetime.datetime.now()
+   for volume in sortedVolumes:
+      lastBackup = datetime.datetime.strptime(volume['lastBackup'],'%Y-%m-%d %H:%M:%S')
+      ageDelta = now - lastBackup
+      currentAge = ageDelta.seconds + (ageDelta.days * 86400)
+      volume['currentAge'] = currentAge
+      log_info('   '+volume['key']+' -> '+('DISABLED' if volume['disabled'] else ( volume['lastBackup']+' = '+ ('%d'%currentAge)) ) )
+
+   # GO THROUGH THE LIST IN ORDER, BACKING UP EACH ONE IF NEEDED
+
+   for volume in sortedVolumes:
+      if volume['currentAge'] < volume['minAge'] : continue
+      if volume['disabled'] : continue
+      do_backup(volume)
+
+   log_info('finished')
+
+   # CLEAN UP
+
+   os.unlink(pidfile)
+
+#-----------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
+
 
