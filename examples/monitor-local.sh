07981d3892b0a68e374c8308fc0b04136da4f8cf
Alan Porter <alan@digit>
2014-02-26 22:28:53 -0500

    monitor on pogo with colored LEDs

--- examples/monitor-local.sh
+++ examples/monitor-local.sh
@@ -1,58 +1,76 @@
 #!/bin/bash
 
 status_file="/var/lib/flashback/status"
+queue_file="/var/lib/flashback/queue"
 
-function led_on () {
-   echo "default-on" > /sys/class/leds/plug\:green\:health/trigger
+function led_red () {
+   echo "none" > /sys/class/leds/status\:green\:health/trigger
+   echo "default-on" > /sys/class/leds/status\:red\:fault/trigger
+}
+
+function led_yellow () {
+   echo "default-on" > /sys/class/leds/status\:green\:health/trigger
+   echo "default-on" > /sys/class/leds/status\:red\:fault/trigger
+}
+
+function led_green () {
+   echo "default-on" > /sys/class/leds/status\:green\:health/trigger
+   echo "none" > /sys/class/leds/status\:red\:fault/trigger
 }
 
 function led_off () {
-   echo "none" > /sys/class/leds/plug\:green\:health/trigger
-}
-
-function led_blink () {
-   count=$1
-   on=$2
-   off=$3
-   for i in $(seq 1 $count) ; do
-      echo "default-on" > /sys/class/leds/plug\:green\:health/trigger
-      sleep $on
-      echo "none" > /sys/class/leds/plug\:green\:health/trigger
-      sleep $off
-   done
+   echo "none" > /sys/class/leds/status\:green\:health/trigger
+   echo "none" > /sys/class/leds/status\:red\:fault/trigger
 }
 
 led_off
 
 while true ; do
-   tmp=$(mktemp)
-   cp $status_file $tmp
-   pid=$(grep '^pid=' $tmp | awk -F= '{print $2}')
-   stat=$(grep '^status=' $tmp | awk -F= '{print $2}')
-   rm $tmp
+   statusFile=$(mktemp)
+   cp $status_file $statusFile
+   queueFile=$(mktemp)
+   cp $queue_file $queueFile
+   pid=$(grep '^pid=' $statusFile | awk -F= '{print $2}')
+   stat=$(grep '^status=' $statusFile | awk -F= '{print $2}')
+   wt=$(grep '^wait=' $statusFile | awk -F= '{print $2}')
+   statusErrs=$(grep -c 'FAILED' $queueFile)
+   statusSuccess=$(grep -c 'SUCCEEDED' $queueFile)
+   rm $statusFile
    if [ $(cat /dev/null /proc/$pid/cmdline 2>/dev/null | grep -c 'flashback') -eq 0 ] ; then
       stat='DOWN'
    fi
    case $stat in
       'DOWN')
-         # not working - three short blips (think "SOS")
-         led_blink 3 0.1 0.3
-         sleep 2
+         # not working - three short red blips (think "SOS")
+         led_red ; sleep 2 ; led_off
          ;;
       'BACKING_UP')
-         # blink very quickly
-         led_blink 2 0.2 0.4
+         # blink yellow twice quickly
+         led_yellow ; sleep 0.2 ; led_off ; sleep 0.4
+         led_yellow ; sleep 0.2 ; led_off ; sleep 0.4
          ;;
       'ROTATING'|'CLEANING')
-         # blink very quickly
-         led_blink 2 0.1 0.2
+         # blink yellow three times very quickly
+         led_yellow ; sleep 0.1 ; led_off ; sleep 0.2
+         led_yellow ; sleep 0.1 ; led_off ; sleep 0.2
+         led_yellow ; sleep 0.1 ; led_off ; sleep 0.2
          ;;
       'IDLE')
-         # blink very slowly
-         led_blink 5 2 1
+         # blink yellow once if any backup succeeded
+         if [[ $statusSuccess -gt 0 ]] ; then
+            led_yellow ; sleep 0.3 ; led_off ; sleep 0.6
+         fi
+         # blink red once if any backup failed
+         if [[ $statusErrs -gt 0 ]] ; then
+            led_red ; sleep 0.3 ; led_off ; sleep 0.6
+         fi
+         # blink green once for each "wait" minute
+         for x in $(seq 1 $wt) ; do
+            led_green ; sleep 0.3 ; led_off ; sleep 0.3
+         done
          ;;
    esac
-   sleep 2
+   sleep 1
 done
 
 
